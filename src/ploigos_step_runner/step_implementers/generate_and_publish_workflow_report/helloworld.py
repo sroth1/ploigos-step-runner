f1a91c4b8a66786a80e0335a5291debab13eb5aa
Matt Hargrave <11786890+matthargrave@users.noreply.github.com>
2021-03-12 16:53:36 -0600

    Hackathon (#126)
    * creating helloworld
    
    * moved file to correct location
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * adding StepResult module
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * upload results to nexus
    
    * upload results to nexus
    
    * fixing bad tab

---
+++ src/ploigos_step_runner/step_implementers/generate_and_publish_workflow_report/helloworld.py
@@ -0,0 +1,74 @@
+"""`StepImplementers` for the `generate_and_publish_workflow_report` step.
+"""
+from ploigos_step_runner import StepImplementer
+from ploigos_step_runner.step_result import StepResult
+
+import sh
+
+
+
+DEFAULT_CONFIG = {
+}
+
+REQUIRED_CONFIG_OR_PREVIOUS_STEP_RESULT_ARTIFACT_KEYS = [
+]
+
+
+class HelloWorld(StepImplementer):  
+    """StepImplementer for the generate-and-publish-workflow-report step for HelloWorld.
+    """
+
+    @staticmethod
+    def step_implementer_config_defaults():
+        """Getter for the StepImplementer's configuration defaults.
+        Returns
+        -------
+        dict
+            Default values to use for step configuration values.
+        Notes
+        -----
+        These are the lowest precedence configuration values.
+        """
+        return DEFAULT_CONFIG
+
+    @staticmethod
+    def _required_config_or_result_keys():
+        """Getter for step configuration or previous step result artifacts that are required before
+        running this step.
+        See Also
+        --------
+        _validate_required_config_or_previous_step_result_artifact_keys
+        Returns
+        -------
+        array_list
+            Array of configuration keys or previous step result artifacts
+            that are required before running the step.
+        """
+
+        return REQUIRED_CONFIG_OR_PREVIOUS_STEP_RESULT_ARTIFACT_KEYS
+
+    def _run_step(self):
+        """Runs the step implemented by this StepImplementer.
+        Returns
+        -------
+        StepResult
+            Object containing the dictionary results of this step.
+        """
+        step_result = StepResult.from_step_implementer(self)
+	    #TODO - Add Logic Here
+        
+        print("Step Implementer Hello World")
+        resultfile = 'results.json'
+        filepath = '/results/'
+        nexusurl = 'https://nexus-devsecops.apps.cluster-801d.801d.example.opentlc.com/repository/container-image-signatures'
+        self.workflow_result.write_results_to_json_file(resultfile)
+        sh.curl(  # pylint: disable=no-member
+            '-sSfv',
+            '-X', 'PUT',
+            '--user', "user:password",
+            '--upload-file',  resultfile,
+            nexusurl + filepath + resultfile,
+            _err_to_out=True,
+            _tee='out'
+            )
+        return step_result
